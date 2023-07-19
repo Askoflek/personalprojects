f0b468035b88b01c4033b685f6679a7b15579a16
Askoflek <skoflekandraz@gmail.com>
2023-07-19 18:10:51 +0200

    Continue implementing API

--- Githubsearchapp/src/scripts/githubAPI.js
+++ Githubsearchapp/src/scripts/githubAPI.js
@@ -6,12 +6,30 @@ const response = await octokit.request("GET /orgs/{org}/repos", {
 });
 
 console.log(response);*/
-import { Octokit } from "octokit";
-
-const octokit = new Octokit({});
-
-try {
-  await octokit.request("GET /octocat", {});
-} catch (e) {
-  console.log(e);
+/*const username = "askoflek";
+async function getUser(username) {
+  const response = await fetch(`https://api.github.com/users/${username}`);
+  const data = await response.json();
+  console.log(data);
 }
+
+getUser();*/
+// Replace 'your-username' with the GitHub username you want to fetch data for
+const username = "askoflek";
+const apiUrl = `https://api.github.com/users/${username}`;
+
+fetch(apiUrl)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    // Here, 'data' will contain the response from the API as a JavaScript object
+    console.log(data);
+    // You can do whatever you want with the data here
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
